433f3766493c70ecf1c79388592f25733fea5b59
JLB246 <65634399+JLB246@users.noreply.github.com>
2020-07-29 15:31:44 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+var favoriteFoods = ['shrimp','pasta','steak','corn','salmon'];
+console.log('My second favorite food is '+ favoriteFoods[1]);
+favoriteFoods[4] = 'chicken';
+console.log('My fifth favorite food is '+ favoriteFoods[4])
+console.log('There are '+ favoriteFoods.length +' items in my favoriteFoods array')
